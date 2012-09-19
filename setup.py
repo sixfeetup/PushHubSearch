4e4c65538ec2d939d3659979d6e9dbd41e9840df
Nolan Brubaker <nolan@nbrubaker.com>
2012-09-19 21:56:34 +0000

    Prepare push-hubsearch 0.4.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requires = [
 
 setup(
     name='push-hubsearch',
-    version='0.3',
+    version='0.4',
     description='push-hubsearch',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
