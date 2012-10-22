d8880a3178d4b7be23485a7a79e981c545db72b9
Nolan Brubaker <nolan@nbrubaker.com>
2012-10-22 12:39:59 +0000

    Bump version for release.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requires = [
 
 setup(
     name='push-hubsearch',
-    version='0.5',
+    version='0.6',
     description='push-hubsearch',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
