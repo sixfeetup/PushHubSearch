6668730021a3626ae211da6fd12132af05f00030
Nolan Brubaker <nolan@nbrubaker.com>
2012-11-18 19:06:55 +0000

    Prepare push-hubsearch 0.13.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requires = [
 
 setup(
     name='push-hubsearch',
-    version='0.12',
+    version='0.13',
     description='push-hubsearch',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
