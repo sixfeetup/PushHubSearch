ac07d53e3a9bb68b14e6c78434426ee58622e8c7
Clayton Parker <me@claytron.com>
2013-04-19 15:39:00 +0000

    Prepare push-hubsearch 0.18.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requires = [
 
 setup(
     name='push-hubsearch',
-    version='0.17',
+    version='0.18',
     description='push-hubsearch',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
