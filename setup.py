d5ac0ea2104197d44ebc2db398f6309893220a36
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2013-11-26 15:43:46 +0000

    Prepare PushHubSearch 0.19.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ requires = [
 
 setup(
     name='PushHubSearch',
-    version='0.18',
+    version='0.19',
     description='PushHubSearch',
     long_description=README + '\n\n' + CHANGES,
     license='LICENSE.txt',
